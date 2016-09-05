d794f8974e7324eb4983008da44b0ebaa2aa275e
Bilal Mawji <bmawji3@gatech.edu>
2016-09-05 15:38:07 -0400

    Update views.py

--- app/general/views.py
+++ app/general/views.py
@@ -4,5 +4,5 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title='Home', user="Admin")
+    return render_template('index.html', title='Home', user="Admin")
 
